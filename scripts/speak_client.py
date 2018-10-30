d41d9fd18f71bdf897102450a04a53a79b0ebfdc
ayakatakuriko <warangkana0601@yahoo.ne.jp>
2018-10-30 09:26:47 +0900

    client完成

--- scripts/speak_client.py
+++ scripts/speak_client.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
 #encoding: utf8
-import subprocess
 import rospy
 from speak_tools.srv import *
+import sys
+
+
+def speak_client(text):
+    rospy.wait_for_service("speak_text")
+    try:
+        speak_text = rospy.ServiceProxy("speak_text", SpeakedText)
+        resp = speak_text(text)
+        return resp.message
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" % e
+
+
+def usage():
+    return "%s <name>" % sys.argv[0]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        text = sys.argv[1]
+        print"Requesting %s" % text
+        print "%s" % (speak_client(text))
+    else:
+        print usage()
