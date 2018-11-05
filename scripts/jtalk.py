a0ca018ac093d6ba6f23970992063fbd7ba335e8
ayakatakuriko <39085632+ayakatakuriko@users.noreply.github.com>
2018-11-05 17:00:58 +0900

    fix

--- scripts/jtalk.py
+++ scripts/jtalk.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 #encoding: utf8
 import subprocess
+import os
 
 
 def jtalk(t):
+    path = os.path.dirname(os.path.realpath(__file__)) + '/open_jtalk.wav'
     open_jtalk = ['open_jtalk']
     mech = ['-x', '/var/lib/mecab/dic/open-jtalk/naist-jdic']
     htsvoice = [
         '-m', '/usr/share/hts-voice/miku/miku.htsvoice']
     speed = ['-r', '1.0']
-    outwav = ['-ow', 'open_jtalk.wav']
+    outwav = ['-ow', path]
     cmd = open_jtalk + mech + htsvoice + speed + outwav
     c = subprocess.Popen(cmd, stdin=subprocess.PIPE)
     c.stdin.write(t)
