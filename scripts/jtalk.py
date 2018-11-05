3160465a99b80626ab73b4300be9fa0b749eacb4
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2018-11-05 17:22:57 +0900

    fix

--- scripts/jtalk.py
+++ scripts/jtalk.py
@@ -5,14 +5,14 @@ import os
 
 
 def jtalk(t):
-    path = os.path.dirname(os.path.realpath(__file__)) + '/open_jtalk.wav'
+    path = os.path.dirname(os.path.realpath(__file__)) + 'scripts/open_jtalk.wav'
     open_jtalk = ['open_jtalk']
     mech = ['-x', '/var/lib/mecab/dic/open-jtalk/naist-jdic']
     htsvoice = [
-        '-m', path + 'scripts/Voice.htsvoice']
-    speed = ['-r', '1.0']
+        '-m', path + 'scripts/tohoku-f01-happy.htsvoice']
+    #speed = ['-r', '1.0']
     outwav = ['-ow', path]
-    cmd = open_jtalk + mech + htsvoice + speed + outwav
+    cmd = open_jtalk + mech + htsvoice + outwav
     c = subprocess.Popen(cmd, stdin=subprocess.PIPE)
     c.stdin.write(t)
     c.stdin.close()
